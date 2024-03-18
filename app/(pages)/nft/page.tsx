13c7b33474ad49c57f7b912bc28e4aca3dde0bc4
Luke Cassady-Dorion <luke@SPStories.com>
2024-03-18 17:34:36 -0300

    Added final group of photos

--- app/(pages)/nft/page.tsx
+++ app/(pages)/nft/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "../../components/Hero"; // Import Hero component
 import NFTView from "../../components/NFTView"; // Import NFTView component
+import { Suspense } from "react";
 
 const Page = () => {
 	return (
 		<div className=" bg-accent">
 			{/* <Hero navbarMode={true} /> Pass a prop to indicate navbar mode */}
-			<NFTView />
+			<Suspense>
+				<NFTView />
+			</Suspense>
 		</div>
 	);
 };
--- app/components/NFTView/NFTView.tsx
+++ app/components/NFTView/NFTView.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react";
 import Hero from "../../components/Hero"; // Adjust the path as necessary
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 interface InfoBoxProps {
 	label: string;
@@ -14,36 +13,34 @@ interface InfoBoxProps {
 
 const InfoBox: React.FC<InfoBoxProps> = ({ label, value, mainColor, accentColor }) => {
 	return (
-		<Suspense>
+		<div
+			className="flex items-start my-1 w-full rounded-lg shadow"
+			style={{
+				borderColor: mainColor,
+				borderWidth: "2px",
+				borderStyle: "solid",
+				boxShadow: `2px 2px ${accentColor}`,
+			}}
+		>
 			<div
-				className="flex items-start my-1 w-full rounded-lg shadow"
+				className="text-center p-1 font-bold rounded-l-md lexend-mega-300"
+				style={{ backgroundColor: mainColor, color: "white" }}
+			>
+				{label}
+			</div>
+			<div
+				className="text-center p-1 font-bold rounded-r-md lexend-mega-300"
 				style={{
-					borderColor: mainColor,
-					borderWidth: "2px",
+					backgroundColor: "white",
+					color: "black",
+					borderLeftColor: mainColor,
+					borderLeftWidth: "2px",
 					borderStyle: "solid",
-					boxShadow: `2px 2px ${accentColor}`,
 				}}
 			>
-				<div
-					className="text-center p-1 font-bold rounded-l-md lexend-mega-300"
-					style={{ backgroundColor: mainColor, color: "white" }}
-				>
-					{label}
-				</div>
-				<div
-					className="text-center p-1 font-bold rounded-r-md lexend-mega-300"
-					style={{
-						backgroundColor: "white",
-						color: "black",
-						borderLeftColor: mainColor,
-						borderLeftWidth: "2px",
-						borderStyle: "solid",
-					}}
-				>
-					{value}
-				</div>
+				{value}
 			</div>
-		</Suspense>
+		</div>
 	);
 };
 
@@ -77,59 +74,55 @@ const NFTView = () => {
 	};
 
 	return (
-		<Suspense>
-			<div className="flex flex-col min-h-screen bg-mainBg" style={{ boxShadow: `0 4px 6px ${accentColor}` }}>
-				{mintSuccess && (
-					<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
-						<div className="w-96 px-8 py-4 bg-main border-4 border-accent shadow-[8px_8px_0px_rgba(34, 160, 147, 1)]">
-							<div>
-								<h1 className="lexend-mega-300 text-2xl mb-4 text-center">
-									Congrats, you{"'"}re one of 42 lucky bears.
-								</h1>
-								<div className="flex justify-center space-x-2 w-full">
-									<button
-										onClick={() => setMintSuccess(false)}
-										className="lexend-mega-300 h-12 border-black border-2 p-2.5 bg-main hover:bg-accent hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF] rounded-full"
-									>
-										Nice!
-									</button>
-								</div>
+		<div className="flex flex-col min-h-screen bg-mainBg" style={{ boxShadow: `0 4px 6px ${accentColor}` }}>
+			{mintSuccess && (
+				<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
+					<div className="w-96 px-8 py-4 bg-main border-4 border-accent shadow-[8px_8px_0px_rgba(34, 160, 147, 1)]">
+						<div>
+							<h1 className="lexend-mega-300 text-2xl mb-4 text-center">Congrats, you{"'"}re one of 42 lucky bears.</h1>
+							<div className="flex justify-center space-x-2 w-full">
+								<button
+									onClick={() => setMintSuccess(false)}
+									className="lexend-mega-300 h-12 border-black border-2 p-2.5 bg-main hover:bg-accent hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF] rounded-full"
+								>
+									Nice!
+								</button>
 							</div>
 						</div>
 					</div>
-				)}
+				</div>
+			)}
 
-				<Hero navbarMode={true} />
-				<div className="flex-grow pt-24 pb-10 px-4 md:px-0">
-					<div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg overflow-hidden flex flex-col md:flex-row">
-						<div className="md:w-1/2 p-4">
-							<img
-								src={`/bearslovemountains-${id}.png`}
-								alt={name}
-								className="h-auto mx-auto rounded-lg"
-								style={{ border: `4px solid ${mainColor}`, boxShadow: `2px 2px ${accentColor}` }}
-							/>
-						</div>
-						<div className="md:w-1/2 p-4 flex flex-col justify-end">
-							<InfoBox label="Name" value={name} mainColor={mainColor} accentColor={accentColor} />
-							<InfoBox label="Symbol" value={symbol} mainColor={mainColor} accentColor={accentColor} />
-							<InfoBox label="Price" value={`${price} BERA`} mainColor={mainColor} accentColor={accentColor} />
-							<button
-								className="lexend-mega-900 h-12 border-2 p-2.5 rounded-full font-bold text-white mt-4 lexend-mega-900"
-								style={{
-									backgroundColor: mainColor,
-									borderColor: accentColor,
-									boxShadow: `2px 2px ${accentColor}`,
-								}}
-								onClick={doMint}
-							>
-								Mint
-							</button>
-						</div>
+			<Hero navbarMode={true} />
+			<div className="flex-grow pt-24 pb-10 px-4 md:px-0">
+				<div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg overflow-hidden flex flex-col md:flex-row">
+					<div className="md:w-1/2 p-4">
+						<img
+							src={`/bearslovemountains-${id}.png`}
+							alt={name}
+							className="h-auto mx-auto rounded-lg"
+							style={{ border: `4px solid ${mainColor}`, boxShadow: `2px 2px ${accentColor}` }}
+						/>
+					</div>
+					<div className="md:w-1/2 p-4 flex flex-col justify-end">
+						<InfoBox label="Name" value={name} mainColor={mainColor} accentColor={accentColor} />
+						<InfoBox label="Symbol" value={symbol} mainColor={mainColor} accentColor={accentColor} />
+						<InfoBox label="Price" value={`${price} BERA`} mainColor={mainColor} accentColor={accentColor} />
+						<button
+							className="lexend-mega-900 h-12 border-2 p-2.5 rounded-full font-bold text-white mt-4 lexend-mega-900"
+							style={{
+								backgroundColor: mainColor,
+								borderColor: accentColor,
+								boxShadow: `2px 2px ${accentColor}`,
+							}}
+							onClick={doMint}
+						>
+							Mint
+						</button>
 					</div>
 				</div>
 			</div>
-		</Suspense>
+		</div>
 	);
 };
 
